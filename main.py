04eba4832227875d03cfe5c977e778de7bb35270
jto-d <jodonnell072@gmail.com>
2023-02-23 12:02:25 -0500

    requirements.txt update

--- main.py
+++ main.py
@@ -1,45 +1,11 @@
 import pywinctl
 import os
 import pyautogui
-import PIL
 import os  # from native modules
-import fitz  # from PyMuPDF
 import pytesseract  # from pytesseract
 import cv2  # from Opencv
-import argparse
-import io  # from native modules
-from PIL import Image, ImageFile  # from Pillow
-from colorama import Fore  # from native modules
-import platform  # from native modules
-import time
+from PIL import Image
 import webbrowser
-
-# get screensize
-# x,y = pyautogui.size()
-# print(f"width={x}\theight={y}")
-
-# x2,y2 = pyautogui.size()
-# x2,y2=int(str(x2)),int(str(y2))
-# print(x2//2)
-# print(y2//2)
-
-# find new window title
-# z1 = pywinctl.getAllTitles()
-# time.sleep(1)
-# print(len(z1))
-# # test with pictures folder
-# os.system("open monitor-1.png")
-# time.sleep(1)
-# z2 = pywinctl.getAllTitles()
-# print((z2))
-# time.sleep(1)
-# z3 = [x for x in z2 if x not in z1]
-# z3 = ''.join(z3)
-# time.sleep(3)
-
-# print(pywinctl.getAllTitles())
-
-# also able to edit z3 to specified window-title string like: "Sublime Text (UNREGISTERED)"
 
 # quarter of screen screensize
 x = 700
@@ -47,12 +13,7 @@ y = 800
 # my.resizeTo(x,y)
 # top-left
 
-# RUN ONLY ON FIRST RUN
-# my = pywinctl.getWindowsWithTitle('Movie Recording')[0]
-# my.moveTo(0, 0)
-# time.sleep(0.1)
-# my.activate()
-# time.sleep(0.1)
+
 
 # save screenshot
 p = pyautogui.screenshot(region=(100,600,x,y))
@@ -67,7 +28,7 @@ p.save(r'p.png')
 # time.sleep(1)
 # my.close()
 
-# TESSERACT#
+# TESSERACT
 
  
 #We then read the image with text
