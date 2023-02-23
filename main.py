1d9e9057163c3409887f7e8585f0594e346fda0a
jto-d <jodonnell072@gmail.com>
2023-02-23 12:04:28 -0500

    refactoring/comments

--- main.py
+++ main.py
@@ -7,43 +7,31 @@ import cv2  # from Opencv
 from PIL import Image
 import webbrowser
 
-# quarter of screen screensize
+# create screensize (run screen_setup.py first)
 x = 700
 y = 800
-# my.resizeTo(x,y)
-# top-left
-
-
 
 # save screenshot
 p = pyautogui.screenshot(region=(100,600,x,y))
 p.save(r'p.png')
 
-# edit screenshot
-# im = PIL.Image.open('p.png')
-# im_crop = im.crop((100, 400, x, y+400))
-# im_crop.save('p.png', quality=100)
-
-# # close window
-# time.sleep(1)
-# my.close()
-
 # TESSERACT
 
- 
-#We then read the image with text
+# read image using cv2
 images=cv2.imread("p.png")
  
-#convert to grayscale image
+# convert to grayscale image
 gray=cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
  
+# use thresh
+
 #checking whether thresh or blur
 # if args["pre_processor"]=="thresh":
 cv2.threshold(gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
 # if args["pre_processor"]=="blur":
 #     cv2.medianBlur(gray, 3)
      
-#memory usage with image i.e. adding image to memory
+# memory usage with image i.e. adding image to memory
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 text = pytesseract.image_to_string(Image.open('57214.png'))
@@ -55,10 +43,11 @@ os.remove(filename)
 # cv2.imshow("Output In Grayscale", gray)
 # cv2.waitKey(0)
 
-# Processing and API Call
+# question processing
 question, answers = text.split('?')
 question = question.replace('\n', ' ') + "?"
 
+# search google
 url = "https://www.google.co.in/search?q=" + question
 webbrowser.open_new(url)
 
